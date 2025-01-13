842ce7d90102f9fbfd26069bf170ee5955a6876e
Andreas Klebe <a.klebe.work@gmail.com>
2025-01-13 15:26:25 +0100

    created 3 state switch and add it to user filter

--- app/Http/Livewire/DataTable/User.php
+++ app/Http/Livewire/DataTable/User.php
@@ -3,6 +3,7 @@
 namespace Modules\Market\app\Http\Livewire\DataTable;
 
 use Illuminate\Database\Eloquent\Builder;
+use Modules\Form\app\Forms\Base\NativeObjectBase;
 use Modules\WebsiteBase\app\Http\Livewire\DataTable\User as WebsiteBaseDataTableUser;
 use Modules\Market\app\Models\User as UserModel;
 
@@ -36,6 +37,47 @@ class User extends WebsiteBaseDataTableUser
     {
         parent::initFilters();
 
+        if ($this->canManage()) {
+            $this->addFilterElement('user_filter_switch_human', [
+                'label'      => 'Humans',
+                'default'    => NativeObjectBase::switch3Unused,
+                'position'   => 1650, // between elements rows and search
+                'soft_reset' => true,
+                'css_group'  => 'col text-center',
+                'css_item'   => '',
+                'view'       => 'data-table::livewire.js-dt.filters.default-elements.3-switch',
+                'builder'    => function (Builder $builder, string $filterElementKey, string $filterValue) {
+                    switch ($filterValue) {
+                        case NativeObjectBase::switch3No:
+                            $builder->mergeConstraintsFrom(UserModel::withNoAclResources(['puppet']));
+                            break;
+                        case NativeObjectBase::switch3Yes:
+                            $builder->mergeConstraintsFrom(UserModel::withAclResources(['puppet']));
+                            break;
+                    }
+                },
+            ]);
+            $this->addFilterElement('user_filter_switch_trader', [
+                'label'      => 'Traders',
+                'default'    => NativeObjectBase::switch3Unused,
+                'position'   => 1651, // between elements rows and search
+                'soft_reset' => true,
+                'css_group'  => 'col text-center',
+                'css_item'   => '',
+                'view'       => 'data-table::livewire.js-dt.filters.default-elements.3-switch',
+                'builder'    => function (Builder $builder, string $filterElementKey, string $filterValue) {
+                    switch ($filterValue) {
+                        case NativeObjectBase::switch3No:
+                            $builder->mergeConstraintsFrom(UserModel::withNoAclResources(['trader']));
+                            break;
+                        case NativeObjectBase::switch3Yes:
+                            $builder->mergeConstraintsFrom(UserModel::withAclResources(['trader']));
+                            break;
+                    }
+                },
+            ]);
+        }
+
         $this->addFilterElement('user_filter1', [
             'label'      => 'Filter',
             'default'    => '',
@@ -46,47 +88,6 @@ class User extends WebsiteBaseDataTableUser
             'options'    => [
                 ''                       => '[No Filter]',
                 ... $this->getFilterOptionsForImages(),
-                'acl_resource_puppet'    => [
-                    'label'   => __('Humans'),
-                    'builder' => function (Builder $builder, string $filterElementKey, string $filterValue) {
-                        $builder->mergeConstraintsFrom(UserModel::withNoAclResources(['puppet']));
-                    },
-                ],
-                'acl_resource_no_puppet' => [
-                    'label'   => __('No Humans'),
-                    'builder' => function (Builder $builder, string $filterElementKey, string $filterValue) {
-                        $builder->mergeConstraintsFrom(UserModel::withAclResources(['puppet']));
-                    },
-                ],
-                'acl_resource_trader'    => [
-                    'label'   => __('Traders'),
-                    'builder' => function (Builder $builder, string $filterElementKey, string $filterValue) {
-                        $builder->mergeConstraintsFrom(UserModel::withAclResources(['trader']));
-                    },
-                ],
-                'acl_resource_no_trader' => [
-                    'label'   => __('No Traders'),
-                    'builder' => function (Builder $builder, string $filterElementKey, string $filterValue) {
-                        $builder->mergeConstraintsFrom(UserModel::withNoAclResources(['trader']));
-                    },
-                ],
-                'filter_is_enabled'      => [
-                    'label'   => __('Enabled'),
-                    'builder' => function (Builder $builder, string $filterElementKey, string $filterValue) {
-                        //$builder->where('id', '<', 10);
-                        $builder->where('is_enabled', '=', true)->where('is_deleted', '=', false);
-                    },
-                ],
-                'filter_is_disabled'     => [
-                    'label'   => __('Disabled'),
-                    'builder' => function (Builder $builder, string $filterElementKey, string $filterValue) {
-                        //$builder->where('id', '<', 10);
-                        $builder->where(function ($q1) {
-                            $q1->where('is_enabled', '=', false)->orWhere('is_deleted', '=', true);
-                        });
-                    },
-                ],
-
             ],
             'view'       => 'data-table::livewire.js-dt.filters.default-elements.select',
         ]);
