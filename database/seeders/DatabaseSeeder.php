e687a1c496c4546713fa413f0b412a202105b290
Andreas Klebe <a.klebe.work@gmail.com>
2025-02-16 19:33:07 +0100

    - fixed mediaItem seeder issue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -45,10 +45,7 @@ class DatabaseSeeder extends BaseModelSeeder
         ]);
 
         // get all new user ids for some relations below
-        $userIds = User::with([])
-                       ->select('id')
-                       ->where('created_at', '>=', $timestamp)
-                       ->pluck('id');
+        $userIds = User::with([])->select('id')->where('created_at', '>=', $timestamp)->pluck('id');
 
 
         // @todo: ac resources to groups ...
@@ -83,25 +80,35 @@ class DatabaseSeeder extends BaseModelSeeder
             // -------------------------------------------------
             // Assign media items to products
             // -------------------------------------------------
-            $mediaItems = MediaItem::where('user_id', $userId)->productImages()->get();
-            if ($mediaItems->count()) {
+            $productMediaItems = MediaItem::where('user_id', $userId)->productImages()->get();
+            if ($productMediaItems->count()) {
                 // Populate the pivot table
-                Product::where('user_id', $userId)->each(function ($product) use ($mediaItems) {
-                    $imagesPerProduct = rand(config('seeders.users.media_items.count_min_product_images_per_product', 2), config('seeders.users.media_items.count_max_product_images_per_product', 2));
-                    // syncWithoutDetaching() better than attach() to avoid errors if items already exists
-                    $product->mediaItems()->syncWithoutDetaching($mediaItems->random($imagesPerProduct)->pluck('id')->toArray());
+                Product::where('user_id', $userId)->each(function ($product) use ($productMediaItems) {
+                    $min = config('seeders.users.media_items.count_min_product_images_per_product', 2);
+                    if ($min > $productMediaItems->count()) {
+                        $min = $productMediaItems->count();
+                    }
+                    $max = config('seeders.users.media_items.count_max_product_images_per_product', 2);
+                    if ($max > $productMediaItems->count()) {
+                        $max = $productMediaItems->count();
+                    }
+                    $imagesPerProduct = rand($min, $max);
+                    if ($imagesPerProduct) {
+                        // syncWithoutDetaching() better than attach() to avoid errors if items already exists
+                        $product->mediaItems()->syncWithoutDetaching($productMediaItems->random($imagesPerProduct)->pluck('id')->toArray());
+                    }
                 });
             }
 
             // -------------------------------------------------
             // Assign media items to user
             // -------------------------------------------------
-            $mediaItems = MediaItem::where('user_id', $userId)->userAvatars()->get();
-            if ($mediaItems->count()) {
+            $avatarMediaItems = MediaItem::where('user_id', $userId)->userAvatars()->get();
+            if ($avatarMediaItems->count()) {
                 // syncWithoutDetaching() better than attach() to avoid errors if items already exists
                 /** @var User $user */
                 $user = User::where('id', $userId)->get()->first();
-                $user->mediaItems()->syncWithoutDetaching($mediaItems->random(rand(1, ($mediaItems->count() >= 5) ? 5 : $mediaItems->count()))->pluck('id')->toArray());
+                $user->mediaItems()->syncWithoutDetaching($avatarMediaItems->random(rand(1, ($avatarMediaItems->count() >= 5) ? 5 : $avatarMediaItems->count()))->pluck('id')->toArray());
             }
 
             // -------------------------------------------------
@@ -113,24 +120,21 @@ class DatabaseSeeder extends BaseModelSeeder
             // -------------------------------------------------
             // Assign aggregated ratings to user
             // -------------------------------------------------
-            AggregatedRating::factory()
-                            ->create([
-                                'model'          => User::class,
-                                'model_id'       => $userId,
-                                'model_sub_code' => 'trust',
-                            ]);
-            AggregatedRating::factory()
-                            ->create([
-                                'model'          => User::class,
-                                'model_id'       => $userId,
-                                'model_sub_code' => 'well_known',
-                            ]);
-            AggregatedRating::factory()
-                            ->create([
-                                'model'          => User::class,
-                                'model_id'       => $userId,
-                                'model_sub_code' => 'offer_success',
-                            ]);
+            AggregatedRating::factory()->create([
+                'model'          => User::class,
+                'model_id'       => $userId,
+                'model_sub_code' => 'trust',
+            ]);
+            AggregatedRating::factory()->create([
+                'model'          => User::class,
+                'model_id'       => $userId,
+                'model_sub_code' => 'well_known',
+            ]);
+            AggregatedRating::factory()->create([
+                'model'          => User::class,
+                'model_id'       => $userId,
+                'model_sub_code' => 'offer_success',
+            ]);
 
         }
     }
@@ -143,9 +147,7 @@ class DatabaseSeeder extends BaseModelSeeder
     private function AssignCategoryRelations(string $timestamp): void
     {
         // preparing for products ...
-        $products = Product::where('created_at', '>=', $timestamp)
-                           ->select('id')
-                           ->get();
+        $products = Product::where('created_at', '>=', $timestamp)->select('id')->get();
         $productsCount = $products->count();
         $minProductsPerCategory = config('seeders.categories.count_min_products', 20);
         if ($minProductsPerCategory > $productsCount) {
@@ -157,9 +159,7 @@ class DatabaseSeeder extends BaseModelSeeder
         }
 
         // preparing for media items ...
-        $mediaItems = MediaItem::where('created_at', '>=', $timestamp)
-                               ->where('object_type', '=', MediaItem::OBJECT_TYPE_CATEGORY_IMAGE)
-                               ->get();
+        $mediaItems = MediaItem::where('created_at', '>=', $timestamp)->where('object_type', '=', MediaItem::OBJECT_TYPE_CATEGORY_IMAGE)->get();
         $mediaItemsCount = $mediaItems->count();
         $minMediaItemsPerCategory = config('seeders.categories.count_min_media_items', 1);
         if ($minMediaItemsPerCategory > $mediaItemsCount) {
@@ -171,39 +171,31 @@ class DatabaseSeeder extends BaseModelSeeder
         }
 
         // Get all new categories ...
-        Category::where('created_at', '>=', $timestamp)
-                ->each(function ($category) use (
-                    $products,
-                    $minProductsPerCategory,
-                    $maxProductsPerCategory,
-                    $minMediaItemsPerCategory,
-                    $maxMediaItemsPerCategory,
-                    $timestamp,
-                    $mediaItems
-                ) {
-                    // -------------------------------------------------
-                    // Assign products to categories
-                    // -------------------------------------------------
-                    if ($minProductsPerCategory) {
-                        // syncWithoutDetaching() better than attach() to avoid errors if items already exists
-                        $category->products()
-                                 ->syncWithoutDetaching($products->random(rand($minProductsPerCategory, $maxProductsPerCategory))
-                                                                 ->select('id')
-                                                                 ->pluck('id')
-                                                                 ->toArray());
-                    }
+        Category::where('created_at', '>=', $timestamp)->each(function ($category) use (
+            $products,
+            $minProductsPerCategory,
+            $maxProductsPerCategory,
+            $minMediaItemsPerCategory,
+            $maxMediaItemsPerCategory,
+            $timestamp,
+            $mediaItems
+        ) {
+            // -------------------------------------------------
+            // Assign products to categories
+            // -------------------------------------------------
+            if ($minProductsPerCategory) {
+                // syncWithoutDetaching() better than attach() to avoid errors if items already exists
+                $category->products()->syncWithoutDetaching($products->random(rand($minProductsPerCategory, $maxProductsPerCategory))->select('id')->pluck('id')->toArray());
+            }
 
-                    // -------------------------------------------------
-                    // Assign media items to categories
-                    // -------------------------------------------------
-                    if ($minMediaItemsPerCategory) {
-                        // syncWithoutDetaching() better than attach() to avoid errors if items already exists
-                        $category->mediaItems()
-                                 ->syncWithoutDetaching($mediaItems->random(rand($minMediaItemsPerCategory, $maxMediaItemsPerCategory))
-                                                                   ->pluck('id')
-                                                                   ->toArray());
-                    }
-                });
+            // -------------------------------------------------
+            // Assign media items to categories
+            // -------------------------------------------------
+            if ($minMediaItemsPerCategory) {
+                // syncWithoutDetaching() better than attach() to avoid errors if items already exists
+                $category->mediaItems()->syncWithoutDetaching($mediaItems->random(rand($minMediaItemsPerCategory, $maxMediaItemsPerCategory))->pluck('id')->toArray());
+            }
+        });
     }
 
     /**
@@ -216,36 +208,29 @@ class DatabaseSeeder extends BaseModelSeeder
         // -------------------------------------------------
         // Assign aggregated ratings to products
         // -------------------------------------------------
-        Product::where('created_at', '>=', $timestamp)
-               ->each(function ($product) {
-                   AggregatedRating::factory()
-                                   ->create([
-                                       'model'          => Product::class,
-                                       'model_id'       => $product->id,
-                                       'model_sub_code' => Product::RATING_SUB_CODE_PUBLIC_PRODUCT,
-                                   ]);
-                   AggregatedRating::factory()
-                                   ->create([
-                                       'model'          => Product::class,
-                                       'model_id'       => $product->id,
-                                       'model_sub_code' => Product::RATING_SUB_CODE_CONDITION,
-                                   ]);
-               });
+        Product::where('created_at', '>=', $timestamp)->each(function ($product) {
+            AggregatedRating::factory()->create([
+                'model'          => Product::class,
+                'model_id'       => $product->id,
+                'model_sub_code' => Product::RATING_SUB_CODE_PUBLIC_PRODUCT,
+            ]);
+            AggregatedRating::factory()->create([
+                'model'          => Product::class,
+                'model_id'       => $product->id,
+                'model_sub_code' => Product::RATING_SUB_CODE_CONDITION,
+            ]);
+        });
 
         // -------------------------------------------------
         // Assign aggregated ratings to categories
         // -------------------------------------------------
-        Category::where('created_at', '>=', $timestamp)
-                ->select('id')
-                ->get()
-                ->each(function ($category) {
-                    AggregatedRating::factory()
-                                    ->create([
-                                        'model'          => Category::class,
-                                        'model_id'       => $category->id,
-                                        'model_sub_code' => 'like',
-                                    ]);
-                });
+        Category::where('created_at', '>=', $timestamp)->select('id')->get()->each(function ($category) {
+            AggregatedRating::factory()->create([
+                'model'          => Category::class,
+                'model_id'       => $category->id,
+                'model_sub_code' => 'like',
+            ]);
+        });
     }
 
 }
